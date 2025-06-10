de93312c889d3b5d811b90f0fb94bcf1cbcb6687
Prit-das <pdas99083@gmail.com>
2025-06-11 01:55:33 +0400

    Carousel added to the hero section

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Hero Carousel
+const sliders = document.querySelectorAll('.carousel');
+const controller = document.querySelectorAll('.bars');
+const track = document.querySelector('.carousel-track');
+// Manually control
+controller.forEach((bar, index) => {
+    bar.addEventListener('click', () => {
+        const slideWidth = sliders[0].clientWidth;
+        bar.style.backgroundColor = "white";
+        track.style.transform = `translateX(-${index * slideWidth}px)`;
+    });
+});
+
 // Tab menu
 let menu = document.querySelector(".propertyOptions");
 let items = menu.querySelectorAll(".item");
